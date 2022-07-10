99b1c73f79e168085a54e8185034191d8e9c7319
Jayesh Verma <jayeshverma73@gmail.com>
2022-07-10 14:45:32 +0530

    intial commit

---
+++ sum.py
@@ -0,0 +1,7 @@
+def add_sum(a,b):
+    total = a + b
+    return total
+
+x = int(input("Enter first number: "))
+y = int(input("Enter Second number: "))
+print("Sum of two numbers is: " , add_sum(x,y))    
