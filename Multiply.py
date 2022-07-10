52e3e921df2c1ef0aecfa04b8c796c98f633adf7
Jayesh Verma <jayeshverma73@gmail.com>
2022-07-10 15:07:46 +0530

    Modified code

---
+++ Multiply.py
@@ -0,0 +1,7 @@
+def multiply(a,b):
+    Mul = a * b
+    return Mul
+
+x = int(input("Enter first number: "))
+y = int(input("Enter Second number: "))
+print("The product of two integer numbers are: " , multiply(x,y))    
---
+++ diff.py
@@ -0,0 +1,7 @@
+def Diff_sub(a,b):
+    Diff = a + b
+    return Diff
+
+x = int(input("Enter first number: "))
+y = int(input("Enter Second number: "))
+print("The Difference of two integer numbers are: " , Diff_sub(x,y))    
